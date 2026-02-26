2a6ef89da06ac318f03435fa9cda0d964e238e44
kaptanoguz <kaptanoguz@gmail.com>
2026-02-26 23:10:51 +0300

    feat: add main electron entry and logo

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        title: "Online Movie Catalog",
+        icon: path.join(__dirname, 'src/logo.png')
+    });
+
+    win.loadFile('src/index.html');
+
+    // Optional: Remove default menu
+    win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
